b93b725216deb648ec15c3659e5652fd71521f40
Frisianrage <65295703+Frisianrage@users.noreply.github.com>
2022-02-10 14:13:18 -0800

    Mongoose schemas finished
    finished the schemas for employer, user, application and files

--- models/applicationModel.js
+++ models/applicationModel.js
@@ -1,12 +1,79 @@
-/*
-jobtitle
-jobdescription
-location
-remote?
-salary
-benefits
-employer
-application date
-source - like linkedin or indeed
-status
-*/
+const mongoose = require('mongoose')
+
+const applicationSchema = mongoose.Schema({
+    jobtitle: {
+        type: String,
+        required: true,
+        default: ""
+    },
+    jobdescription: {
+        type: String,
+        required: false,
+        default: ""
+    },
+    salary: {
+        type: String,
+        required: false,
+        default: ""
+    },
+    remote: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    location: {
+        city: {
+                type: String,
+                required: false 
+                },
+        state: {
+                type: String,
+                required: false 
+                },
+        country: {
+                type: String,
+                required: false 
+                }
+    },
+    Employer: {
+        Employer: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'Employer',
+            default: null 
+                },
+        contactperson: {
+            name:   {type: String,
+                    required: false 
+                    },
+            email:  {type: String,
+                    required: false 
+                    },
+            phone:  {type: String,
+                    required: false 
+                    }
+        },
+    },
+    coverletter: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: false,
+        ref: 'File',
+        default: null
+    },
+    status: {
+        type: String,
+        required: true,
+        default: "Open"
+    },
+    source: {
+        type: String,
+        required: false,
+        default: ""
+    },
+}, {
+    timestamps: true
+})
+
+const Application = mongoose.model('Application', applicationSchema)
+
+module.exports = Application
--- models/employerModel.js
+++ models/employerModel.js
@@ -1,10 +1,44 @@
-/*
-Company Name
-Address
-ZIP
-City
-telephone
-email
-contact person
-job ad
-*/
+const mongoose = require('mongoose')
+
+const employerSchema = mongoose.Schema({
+    companyname: {
+        type: String,
+        required: true,
+        default: ""
+    },
+    address: {
+        street: {type: String,
+                required: false 
+                },
+        house_no: {type: String,
+                required: false 
+                },
+        zip_code: {type: String,
+                required: false 
+                },
+        city: {type: String,
+                required: false 
+                },
+        country: {type: String,
+                required: false 
+                }
+    },
+    email: {
+        type: String,
+        required: false
+    },
+    telephone: { 
+        type: String, 
+        required: false
+    },
+    mobile: {
+        type: String, 
+        required: false
+    }
+}, {
+    timestamps: true
+})
+
+const Employer = mongoose.model('Employer', employerSchema)
+
+module.exports = Employer
