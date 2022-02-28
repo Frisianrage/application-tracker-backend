35456eac58be96f709c6157e5b47ee8e6bb309fc
Frisianrage <65295703+Frisianrage@users.noreply.github.com>
2022-02-27 18:56:29 -0800

    added cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const cors = require('cors')
 const connectDB = require('./config/db')
 const userRoutes = require('./routes/userRoutes')
 const employerRoutes = require('./routes/employerRoutes')
@@ -11,6 +12,7 @@ connectDB()
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use(cors())
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 
